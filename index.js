addf6513c1ca5e628d49204dfb86f52c3af64f03
Luna Seifert <thepuddingdeity@gmail.com>
2022-09-30 11:50:15 -0600

    commit tree fd4a13086df9f2251102f3f6d0f949eab085accd

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(nameBase, specificName){
+    let tempName = specificName.toLowerCase();
+    let results = nameBase.filter(item => item.toLowerCase() === tempName)
+    return results
+}
+
+function fuzzyMatch(nameBase, fuzzyName){
+    return nameBase.filter(item => item.startsWith(fuzzyName))
+}
+
+function matchName(array, name){
+    return array.filter(item => item.name === name)
+}
